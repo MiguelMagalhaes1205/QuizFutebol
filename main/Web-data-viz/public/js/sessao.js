0a3e75456f5d3b8269b15b2f401f2dfbbd789425
MiguelMagalhaes1205 <miguel.magalhaes@sptech.school>
2025-06-02 14:47:24 -0300

    Começo dashboard

--- main/Web-data-viz/public/js/sessao.js
+++ main/Web-data-viz/public/js/sessao.js
@@ -2,11 +2,14 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
+    var timefavorito = sessionStorage.TIME_FAVORITO;
 
     var b_usuario = document.getElementById("b_usuario");
+    var b_time_favorito = document.getElementById("b_time_favorito");
 
-    if (email != null && nome != null) {
+    if (email != null && nome != null && timefavorito != null) {
         b_usuario.innerHTML = nome;
+        b_time_favorito.innerHTML = timefavorito;
     } else {
         window.location = "../login.html";
     }
