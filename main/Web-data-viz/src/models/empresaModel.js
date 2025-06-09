d445a977aea8f78d8b0b2978db0e9dafbc545e31
MiguelMagalhaes1205 <miguel.magalhaes@sptech.school>
2025-06-08 21:48:23 -0300

    Deletando arquivos inúteis

--- main/Web-data-viz/src/models/empresaModel.js
+++
@@ -1,9 +0,0 @@
-var database = require("../database/config");
-
-function buscarPorId(id) {
-  var instrucaoSql = `SELECT * FROM usuario WHERE id = '${id}'`;
-
-  return database.executar(instrucaoSql);
-}
-
-module.exports = {buscarPorId};
